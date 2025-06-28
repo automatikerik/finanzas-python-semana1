a14e33f200c8d1067bcdedbf9fb65ce22ebabd3f
automatikerik <erikdaniel.rodriguez05@gmail.com>
2025-06-28 17:04:34 -0600

    Add files via upload
    Agrego versión avanzada del script con lógica condicional (Clase 2)

---
+++ finanzas_avanzado.py
@@ -0,0 +1,21 @@
+ingreso = float(input("¿Cuanto cobraste esta semana? "))
+
+necesidades = ingreso * 0.50
+deseos = ingreso * 0.30
+ahorro = ingreso * 0.20
+
+print("\nDistribucion 50-30-20")
+print(f"Necesidades: ${necesidades:.2f}")
+print(f"Deseos: ${deseos:.2f}")
+print(f"Ahorro: ${ahorro:.2f}")
+
+print("\nAnalisis de Ingreso:")
+if ingreso < 1000:
+    print("Mejora ingreso de efectivo.")
+elif ingreso <= 3000:
+    print("Ingreso aceptable pero se puede mejorar")
+elif ingreso <= 5000:
+    print("Buen ingreso.")
+else:
+    print("Excelente ingreso")
+
